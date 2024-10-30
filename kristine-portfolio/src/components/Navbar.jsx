c29a9959adea1009b9b3c99b71abeda08f2985cc
KrisVill <145520691+KrisVill@users.noreply.github.com>
2024-10-30 20:57:56 +0800

    Update Navbar.jsx

--- kristine-portfolio/src/components/Navbar.jsx
+++ kristine-portfolio/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
     <section className='nav'>
         <div className="center-body">
             <div className="logo">
-                <h1>Kristine</h1>
+                <h1>KV</h1>
             </div>
             <div className="menu">
                 <ul>
